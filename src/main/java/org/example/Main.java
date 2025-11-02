176ccd29f949c904bb67a45d3a3e4c7da1bebba2
IsFariza <fariza.ismag@gmail.com>
2025-11-03 00:11:53 +0500

    add main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,9 @@
 package org.example;
 
+import org.example.IOHandler.OutputWriter;
+
 public class Main {
+    public static void main(String[] args) {
+        OutputWriter.writeAllGraphsOutput("output.json");
+    }
 }
